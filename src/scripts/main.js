3c7f0b73a433ae9edf5bc9ff3865b69393cfcc53
Mateusz <mateusz.ruszczynski@gmail.com>
2026-02-24 19:30:33 +0100

    add js script for contact block

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -9,3 +9,26 @@ const handleMenuScroll = () => {
 window.addEventListener('hashchange', handleMenuScroll);
 
 handleMenuScroll();
+
+const contactForm = document.getElementById('contact-form');
+
+if (contactForm) {
+  contactForm.addEventListener('submit', function (event) {
+    event.preventDefault();
+    // const submitBtn = contactForm.querySelector('.contact__button');
+    // submitBtn.textContent = 'Message sent!';
+    // submitBtn.style.backgroundColor = '#292929';
+    // submitBtn.style.color = '#ffffff';
+    // submitBtn.disabled = true;
+    // contactForm.reset();
+    const contactContainer = document.querySelector('.contact');
+    contactContainer.innerHTML = `
+    <div style="text-align: left; animation: fadeIn 0.5s;">
+      <h2 class="title">Thank you!</h2>
+      <p style="font-family: Inter, sans-serif; margin-top: 10px;">
+        Your message has been sent. We will get back to you soon.
+      </p>
+    </div>
+  `;
+  });
+}
